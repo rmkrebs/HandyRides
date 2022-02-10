510f21c1608f19d6198a8b782635ce79d8426683
mjstroebel <70414872+mjstroebel@users.noreply.github.com>
2022-02-09 21:07:54 -0500

    Update urls.py

--- ORF401 Handy Rides/HandyRides/HandyRides/urls.py
+++ ORF401 Handy Rides/HandyRides/HandyRides/urls.py
@@ -18,7 +18,7 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('rides/', include('rides.urls')),
     path('', TemplateView.as_view(template_name="index.html"))
+    path('admin/', admin.site.urls),
+    path('rides/', include('rides.urls'))
 ]
