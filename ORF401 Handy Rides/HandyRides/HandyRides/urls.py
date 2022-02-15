d998a9f94214e5f5903a40fd296a304c854947ca
cassiereale <99140591+cassiereale@users.noreply.github.com>
2022-02-14 20:26:09 -0500

    logo

--- ORF401 Handy Rides/HandyRides/HandyRides/urls.py
+++ ORF401 Handy Rides/HandyRides/HandyRides/urls.py
@@ -21,6 +21,6 @@ from django.views.generic import TemplateView
 urlpatterns = [
     path('', TemplateView.as_view(template_name="index.html")),
     path('admin/', admin.site.urls),
-    path('rides/', include('rides.urls'),
+    path('rides/', include('rides.urls')),
     path('', TemplateView.as_view(template_name="index.html"))
 ]
