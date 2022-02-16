6b1e91a099a7a186cc9b2d45e2627a811a6d1688
rmkrebs <63682886+rmkrebs@users.noreply.github.com>
2022-02-15 23:12:38 -0500

    nothing new

--- ORF401 Handy Rides/HandyRides/rides/forms.py
+++ ORF401 Handy Rides/HandyRides/rides/forms.py
@@ -2,5 +2,6 @@ from django import forms
 
 
 class RideForm(forms.Form):
-  search_City = forms.CharField(label='Search by City', max_length=64, required = False)
-  search_State = forms.CharField(label='Search State', max_length=2, required = False)
+ search_City = forms.CharField(label='Search by City', max_length=64, required = False)
+ search_State = forms.CharField(label='Search State', max_length=2, required = False)
+
