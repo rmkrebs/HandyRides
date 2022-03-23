263486883fb5ef56e66cd0191981931d9efcb99a
cassiereale <99140591+cassiereale@users.noreply.github.com>
2022-03-22 20:02:14 -0400

    hello

--- ORF401 Handy Rides/HandyRides/rides/forms.py
+++ ORF401 Handy Rides/HandyRides/rides/forms.py
@@ -3,10 +3,10 @@ from .models import Person
 
 
 class RideForm(forms.Form):
-  search = forms.CharField(label='Search term', max_length=64, required=False)
-
+  search_City = forms.CharField(label='Search by City', max_length=64, required = False)
+  search_State = forms.CharField(label='Search State', max_length=2, required = False)
 
 class NewRideForm(forms.ModelForm):
-  class Meta:
-    model = Person
-    exclude = []
+    class Meta:
+        model = Person
+        exclude = []
--- ORF401 Handy Rides/HandyRides/rides/views.py
+++ ORF401 Handy Rides/HandyRides/rides/views.py
@@ -35,9 +35,12 @@ def index(request):
   context["new_ride_form"] = NewRideForm()
 
   return render(request, "index_view.html", context)
-  
+
 def create(request):
+    context ={}
+    context["new_ride_form"] = NewRideForm()
     if request.method == "POST":
         new_ride = NewRideForm(request.POST)
         new_ride.save()
-    return redirect("/rides")
+        return redirect("/addride")
+    return render(request,"addride.html", context)
